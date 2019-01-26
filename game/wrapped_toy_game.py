4e329ac4ed07139f63e5778ae9dec70bbaa43d10
patrick-12sigma <pliu@12sigma.ai>
2019-01-26 01:29:22 -0800

    Implementing env (WIP)

--- game/wrapped_toy_game.py
+++ game/wrapped_toy_game.py
@@ -2,7 +2,7 @@
 This game engine simulates a segmentation tracing game environment (env) for a DRL agent (agent).
 
 Overview:
-    - The agent can take four actions, UP, DOWN, LEFT and RIGHT.
+    - The agent is a dot with position (y, x). It can take 4 + 1 actions, UP, DOWN, LEFT and RIGHT, and DO NOTHING.
 
     - The env takes an image and a label, and a list of seed points to initialize.
       The env returns the image of the next step, the reward for the action and if
@@ -18,155 +18,174 @@ Details:
     - If the agent is out of the segmentation for MAX_STRAY slices, then the game is over, and gets a reward of -10.
 
 """
-import numpy as np
-import sys
-import random
+import SimpleITK as sitk
+import scipy
 import pygame
-from game import flappy_bird_utils
-import pygame.surfarray as surfarray
+import numpy as np
 from pygame.locals import *
-from itertools import cycle
 
-SCREENHEIGHT = 128
-SCREENWIDTH = 128
+
+SCREENHEIGHT = 512
+SCREENWIDTH = 512
+PATCHHEIGHT = 128
+PATCHWIDTH = 128
+MAX_STRAY = 5
+STEP_SIZE = 1 # number of pixels per move
 
 pygame.init()
 FPSCLOCK = pygame.time.Clock()
 SCREEN = pygame.display.set_mode((SCREENWIDTH, SCREENHEIGHT))
 pygame.display.set_caption('Toy Game')
 
-image = '/data/pliu/test/0_DICOMC_PA10_ST0_SE3.nii.gz'
-label = '/data/pliu/test/0_DICOMC_PA10_ST0_SE3.post.seg.nii.gz'
 
-MAX_STRAY = 1
+def load():
+    image = '/data/pliu/test/rib_nii/0_DICOMC_PA10_ST0_SE3.nii.gz'
+    label = '/data/pliu/test/rib_nii/0_DICOMC_PA10_ST0_SE3.post.seg.nii.gz'
+    # in the order of z, y, x
+    images = sitk.GetArrayFromImage(sitk.ReadImage(image))[::-1, ...]
+    labels = sitk.GetArrayFromImage(sitk.ReadImage(label))[::-1, ...]
+    return images, labels
 
-IMAGES, SOUNDS, HITMASKS = flappy_bird_utils.load()
-'''
-PIPEGAPSIZE is set to 100 as hard mode, 150 as medium and 200 as easy
-https://github.com/yenchenlin/DeepLearningFlappyBird/issues/57
-'''
-# PIPEGAPSIZE = 100 # gap between upper and lower part of pipe
-BASEY = SCREENHEIGHT * 0.79
 
-PLAYER_WIDTH = IMAGES['player'][0].get_width()
-PLAYER_HEIGHT = IMAGES['player'][0].get_height()
-PIPE_WIDTH = IMAGES['pipe'][0].get_width()
-PIPE_HEIGHT = IMAGES['pipe'][0].get_height()
-BACKGROUND_WIDTH = IMAGES['background'].get_width()
+def get_2d_centroid(image_array):
+    assert len(image_array.shape) == 2
+    y, x = scipy.ndimage.measurements.center_of_mass(image_array)
+    return y, x
 
-PLAYER_INDEX_GEN = cycle([0, 1, 2, 1])
+
+def get_init_agent_position(labels, cc_id=None, mode='random'):
+    """Get the seed position in a new tracing episode
+
+    Args:
+        mode: random or top
+
+    Returns:
+
+    """
+    if cc_id is None:
+        cc_id = np.random.choice(np.unique(labels[labels > 0]))
+    assert cc_id > 0
+    binary_mask = (labels == cc_id)
+    num_z = binary_mask.shape[0]
+    sum_z = binary_mask.reshape([num_z, -1]).sum(axis=1)
+    if mode == 'random':
+        pos_z = np.random.choice(np.where(sum_z > 0)[0])
+    elif mode == 'top':
+        pos_z = np.min(np.where(sum_z > 0)[0])
+    label_slice = binary_mask[pos_z, ...]
+    pos_y, pos_x = get_2d_centroid(label_slice)
+    return pos_z, pos_y, pos_x
+
+
+def get_cc_mask(label_slice, center_yx):
+    y, x = center_yx
+    cc_id = label_slice[y, x]
+    cc_mask = (label_slice == cc_id)
+    return cc_mask
+
+
+def crop_around_point(image_array, center_yx, target_shape):
+    """Center crop an image array around a point
+
+    Args:
+        image_array:
+        center_yx:
+        target_shape:
+
+    Returns:
+
+    """
+    pad_y, pad_x = ((np.array(target_shape) + 1) // 2).astype(np.int)
+    pad_width = ((pad_y, pad_y), (pad_x, pad_x))
+    image_array = np.pad(image_array, pad_width=pad_width, mode='constant')
+    ymin, xmin = (np.array(center_yx) + np.array([pad_y, pad_x]) - np.array(target_shape) // 2).astype(np.int)
+    ymax, xmax = (np.array([ymin, xmin]) + np.array(target_shape)).astype(np.int)
+    cropped_array = image_array[ymin:ymax, xmin:xmax]
+    assert cropped_array.shape == tuple(target_shape)
+    return cropped_array
+
+
+images, labels = load()
 
 
 class GameState:
-    def __init__(self, show_play=True, FPS=30, game_level='hard'):
-        self.score = self.playerIndex = self.loopIter = 0
-        self.playerx = int(SCREENWIDTH * 0.2)
-        self.playery = int((SCREENHEIGHT - PLAYER_HEIGHT) / 2)
-        self.basex = 0
-        self.baseShift = IMAGES['base'].get_width() - BACKGROUND_WIDTH
-        self.game_level = game_level
+    def __init__(self, images, labels, show_play=True, FPS=30):
+        self.score = 0
+        # 3d images
+        self.images = images
+        self.labels = labels
 
-        newPipe1 = getRandomPipe(self.game_level)
-        newPipe2 = getRandomPipe(self.game_level)
-        self.upperPipes = [
-            {'x': SCREENWIDTH, 'y': newPipe1[0]['y']},
-            {'x': SCREENWIDTH + (SCREENWIDTH / 2), 'y': newPipe2[0]['y']},
-        ]
-        self.lowerPipes = [
-            {'x': SCREENWIDTH, 'y': newPipe1[1]['y']},
-            {'x': SCREENWIDTH + (SCREENWIDTH / 2), 'y': newPipe2[1]['y']},
-        ]
+        # get initial position of agent
+        self.playerz, self.playery, self.playerx = get_init_agent_position(labels, mode='random')
+        self.playeryx = (self.playery, self.playerx)
 
-        # player velocity, max velocity, downward accleration, accleration on flap
-        self.pipeVelX = -4
-        self.playerVelY    =  0    # player's velocity along Y, default same as playerFlapped
-        self.playerMaxVelY =  10   # max vel along Y, max descend speed
-        self.playerMinVelY =  -8   # min vel along Y, max ascend speed
-        self.playerAccY    =   1   # players downward accleration
-        self.playerFlapAcc =  -9   # players speed on flapping
-        self.playerFlapped = False # True when player flaps
+        # crop patch
+        self.image_slice = self.images[self.playerz, ...]
+        self.label_slice = get_cc_mask(self.labels[self.playerz, ...], self.playeryx)
+        self.patch = self.crop(self.image_slice, self.playeryx)
+        self.patch_mask = self.crop(self.label_slice, (self.playery, self.playerx))
+
+        self.stray_counter = 0
         self.show_play = show_play
         self.FPS = FPS
 
+    @staticmethod
+    def crop(image_slice, center_yx):
+        cropped_array = crop_around_point(image_slice, center_yx, target_shape=(PATCHHEIGHT, PATCHWIDTH))
+        return cropped_array
+
     def frame_step(self, input_actions):
+        """
+
+        Args:
+            input_actions: one hot vector of length 5
+
+        Returns:
+
+        """
         pygame.event.pump()
 
-        reward = 0.1
+        reward = 1
         terminal = False
 
         if sum(input_actions) != 1:
             raise ValueError('Multiple input actions!')
 
         # input_actions[0] == 1: do nothing
-        # input_actions[1] == 1: flap the bird
+        # input_actions[1] == 1: UP,    y - 1
+        # input_actions[2] == 1: DOWN,  y + 1
+        # input_actions[3] == 1: LEFT,  x - 1
+        # input_actions[4] == 1: RIGHT, x + 1
         if input_actions[1] == 1:
-            if self.playery > -2 * PLAYER_HEIGHT:
-                self.playerVelY = self.playerFlapAcc
-                self.playerFlapped = True
-                #SOUNDS['wing'].play()
+            self.playery -= 1
+        if input_actions[2] == 1:
+            self.playery += 1
+        if input_actions[3] == 1:
+            self.playerx -= 1
+        if input_actions[4] == 1:
+            self.playerx += 1
 
         # check for score
-        playerMidPos = self.playerx + PLAYER_WIDTH / 2
-        for pipe in self.upperPipes:
-            pipeMidPos = pipe['x'] + PIPE_WIDTH / 2
-            if pipeMidPos <= playerMidPos < pipeMidPos + 4:
-                self.score += 1
-                #SOUNDS['point'].play()
-                reward = 1
-
-        # playerIndex basex change
-        if (self.loopIter + 1) % 3 == 0:
-            self.playerIndex = next(PLAYER_INDEX_GEN)
-        self.loopIter = (self.loopIter + 1) % 30
-        self.basex = -((-self.basex + 100) % self.baseShift)
+        self.score += 1
+        reward = 1
 
         # player's movement
-        if self.playerVelY < self.playerMaxVelY and not self.playerFlapped:
-            self.playerVelY += self.playerAccY
-        if self.playerFlapped:
-            self.playerFlapped = False
         self.playery += min(self.playerVelY, BASEY - self.playery - PLAYER_HEIGHT)
-        if self.playery < 0:
-            self.playery = 0
 
-        # move pipes to left
-        for uPipe, lPipe in zip(self.upperPipes, self.lowerPipes):
-            uPipe['x'] += self.pipeVelX
-            lPipe['x'] += self.pipeVelX
-
-        # add new pipe when first pipe is about to touch left of screen
-        if 0 < self.upperPipes[0]['x'] < 5:
-            newPipe = getRandomPipe(self.game_level)
-            self.upperPipes.append(newPipe[0])
-            self.lowerPipes.append(newPipe[1])
-
-        # remove first pipe if its out of the screen
-        if self.upperPipes[0]['x'] < -PIPE_WIDTH:
-            self.upperPipes.pop(0)
-            self.lowerPipes.pop(0)
-
-        # check if crash here
-        isCrash= checkCrash({'x': self.playerx, 'y': self.playery,
-                             'index': self.playerIndex},
-                            self.upperPipes, self.lowerPipes)
-        if isCrash:
-            #SOUNDS['hit'].play()
-            #SOUNDS['die'].play()
-            terminal = True
-            # init with the old parameters
-            self.__init__(FPS=self.FPS, show_play=self.show_play, game_level=self.game_level)
-            reward = -1
+        # check if the point is stray (outside mask)
+        is_stray = check_stray({'x': self.playerx, 'y': self.playery}, self.cc_mask)
+        if is_stray:
+            self.stray_counter += 1
+            if self.stray_counter >= MAX_STRAY:
+                terminal = True
+                # init with the old parameters
+                self.__init__(FPS=self.FPS, show_play=self.show_play, game_level=self.game_level)
+                reward = -1
+        else:
+            self.stray_counter = 0
 
         # draw sprites
-        SCREEN.blit(IMAGES['background'], (0,0))
-
-        for uPipe, lPipe in zip(self.upperPipes, self.lowerPipes):
-            SCREEN.blit(IMAGES['pipe'][0], (uPipe['x'], uPipe['y']))
-            SCREEN.blit(IMAGES['pipe'][1], (lPipe['x'], lPipe['y']))
-
-        SCREEN.blit(IMAGES['base'], (self.basex, BASEY))
-        # print score so player overlaps the score
-        # showScore(self.score)
+        SCREEN.blit(IMAGES['background'], (0, 0))
         SCREEN.blit(IMAGES['player'][self.playerIndex],
                     (self.playerx, self.playery))
 
@@ -175,63 +194,12 @@ class GameState:
             pygame.display.update()
         if self.FPS > 0:
             FPSCLOCK.tick(self.FPS)
-        #print self.upperPipes[0]['y'] + PIPE_HEIGHT - int(BASEY * 0.2)
+        # print self.upperPipes[0]['y'] + PIPE_HEIGHT - int(BASEY * 0.2)
         return image_data, reward, terminal
 
 
-def getPipeGapSize(game_level='hard'):
-    '''
-    PIPEGAPSIZE is set to 100 as hard mode, 150 as medium and 200 as easy
-    https://github.com/yenchenlin/DeepLearningFlappyBird/issues/57
-    '''
-    PIPEGAPSIZE = 100  # gap between upper and lower part of pipe
 
-    if game_level.lower() == 'hard':
-        pipe_gap_size = 100
-    elif game_level.lower() == 'medium':
-        pipe_gap_size = 150
-    elif game_level.lower() == 'easy':
-        pipe_gap_size = 200
-    else:
-        raise ValueError('Game level can only be easy, medium or hard')
-    return pipe_gap_size
-
-
-def getRandomPipe(game_level='hard'):
-    """returns a randomly generated pipe"""
-    # get pipe_gap_size based on game level
-    pipe_gap_size = getPipeGapSize(game_level=game_level)
-
-    # y of gap between upper and lower pipe
-    gapYs = [20, 30, 40, 50, 60, 70, 80, 90]
-    index = random.randint(0, len(gapYs)-1)
-    gapY = gapYs[index]
-
-    gapY += int(BASEY * 0.2)
-    pipeX = SCREENWIDTH + 10
-
-    return [
-        {'x': pipeX, 'y': gapY - PIPE_HEIGHT},  # upper pipe
-        {'x': pipeX, 'y': gapY + pipe_gap_size},  # lower pipe
-    ]
-
-
-def showScore(score):
-    """displays score in center of screen"""
-    scoreDigits = [int(x) for x in list(str(score))]
-    totalWidth = 0 # total width of all numbers to be printed
-
-    for digit in scoreDigits:
-        totalWidth += IMAGES['numbers'][digit].get_width()
-
-    Xoffset = (SCREENWIDTH - totalWidth) / 2
-
-    for digit in scoreDigits:
-        SCREEN.blit(IMAGES['numbers'][digit], (Xoffset, SCREENHEIGHT * 0.1))
-
-
-
-def checkCrash(player, upperPipes, lowerPipes):
+def check_stray(player, upperPipes, lowerPipes):
     """returns True if player collders with base or pipes."""
     pi = player['index']
     player['w'] = IMAGES['player'][0].get_width()
@@ -243,7 +211,7 @@ def checkCrash(player, upperPipes, lowerPipes):
     else:
 
         playerRect = pygame.Rect(player['x'], player['y'],
-                      player['w'], player['h'])
+                                 player['w'], player['h'])
 
         for uPipe, lPipe in zip(upperPipes, lowerPipes):
             # upper and lower pipe rects
@@ -264,18 +232,3 @@ def checkCrash(player, upperPipes, lowerPipes):
 
     return False
 
-def pixelCollision(rect1, rect2, hitmask1, hitmask2):
-    """Checks if two objects collide and not just their rects"""
-    rect = rect1.clip(rect2)
-
-    if rect.width == 0 or rect.height == 0:
-        return False
-
-    x1, y1 = rect.x - rect1.x, rect.y - rect1.y
-    x2, y2 = rect.x - rect2.x, rect.y - rect2.y
-
-    for x in range(rect.width):
-        for y in range(rect.height):
-            if hitmask1[x1+x][y1+y] and hitmask2[x2+x][y2+y]:
-                return True
-    return False
